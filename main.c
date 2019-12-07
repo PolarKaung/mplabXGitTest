51172d6bca0974af3306669e0d687eb5f84dd037
polar <km3684629@gmail.com>
2019-12-07 09:35:00 -0500

    this is initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * File:   main.c
+ * Author: Microchip DeveloperHelp
+ *
+ * Created on October 24, 2018, 3:12 PM
+ */
+
+
+#include <xc.h>
+
+
+void main(void) {
+    TRISA = 0; //All PortC Outputs
+    ////
+    while (1)
+    {
+     
+        RA4 = 1; //LED On
+       
+    }  
+}
